e081cb250f800ddfb5b0ea985846f677b376340f
Warda Azzahra <73280193+WardaZr@users.noreply.github.com>
2022-05-04 16:11:29 +0700

    Add files via upload

---
+++ Views/admin/sadmin-dashboard.php
@@ -0,0 +1,66 @@
+<?php include 'master/head.php'; ?>
+| Dashboard </title><head>
+<?php include 'master/sidebar.php'; ?>
+
+<div class="content-wrapper">
+     <div class="container">
+          <div class="card" style="margin-top: 50px;">
+               <div class="card-body">
+                    <p style="font-size:30px; color: #5F5F5F;">
+                        <i class="fas fa-chart-pie"></i>
+                        <b>Dashboard</b>
+                        <hr>
+                    </p>
+                    <div class="row">
+                         <div class="col-sm-5" style="background-color: #F8BE2C;">
+                              <div class="" style="background-color: #F8BE2C;">
+                                   <div class="card-body">
+                                        <table cellpadding=7>
+                                             <tr>
+                                                  <td rowspan="2">
+                                                       <h5><i class="fas fa-users"></i></h5>
+                                                  </td>
+                                             </tr>
+                                             <tr>
+                                                  <td>
+                                                       <h5 class="card-title">Total Peserta:</h5>
+                                                  </td>
+                                             </tr>
+                                        </table>
+                                        <h1>30000</h1>
+
+                                   </div>
+                              </div>
+                         </div>
+                         <div class="col-sm-1" style="background-color: white; color:white;">
+                              <div class="" style="background-color: white;">
+                                   <div class="card-body">
+                                   </div>
+                              </div>
+                         </div>
+                         <div class="col-sm-5" style="background-color: #01872F; color:white;">
+                              <div class="" style="background-color: #01872F;">
+                                   <div class="card-body">
+                                        <table cellpadding=7>
+                                             <tr>
+                                                  <td rowspan="2">
+                                                       <h5><i class="fas fa-user-tie"></i></h5>
+                                                  </td>
+                                             </tr>
+                                             <tr>
+                                                  <td>
+                                                       <h5 class="card-title">Total Penyelenggara:</h5>
+                                                  </td>
+                                             </tr>
+                                        </table>
+                                        <h1>500</h1>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </div>
+</div>
+
+<?php include 'master/footer.php'; ?>
---
+++ Views/admin/sadmin-index.php
@@ -0,0 +1,23 @@
+<?php include 'master/head.php'; ?>
+| Home </title><head>
+<?php include 'master/sidebar.php'; ?>
+
+<div class="content-wrapper">
+     <div class="container">
+          <div class="card" style="margin-top: 50px;">
+               <div class="card-body">
+                    <p style="font-size:30px; color: #5F5F5F;">
+                         <i class="fas fa-home"></i>
+                         <b>Beranda</b>
+                         <hr>
+                    </p>
+                    <p style="font-size:25px;">
+                         Selamat datang di halaman Admin Agent Contest<br>
+                         <b>Anda login sebagai Super Admin</b>
+                    </p>
+               </div>
+          </div>
+     </div>
+</div> 
+
+<?php include 'master/footer.php'; ?>
